f9d2bc7004991def0a6e46954e39b0d524a61cfa
渔童 <erlscript@gmail.com>
2019-12-06 00:11:56 +0800

    a missing file

---
+++ scenesData/foo.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  backgroundImg: 'scenes/foo.png',
+  width: 2560,
+  height: 1440,
+  asides: '',
+
+  actors: [],
+  affectors: [],
+  items: [],
+  obstacles: [],
+};
